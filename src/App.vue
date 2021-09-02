4cb8800c323918f518e4f5d4fb14377c45235352
lwn-ysy <631955411@qq.com>
2021-09-02 20:59:04 +0800

    vue-cli与vue的学习

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,125 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoHeader :addTodo="addTodo"></TodoHeader>
+    <TodoList
+      :todos="todos"
+      :checkTodo="checkTodo"
+      :deleteItem="deleteItem"
+    ></TodoList>
+    <TodoFooter
+      :todos="todos"
+      :checkAllTodos="checkAllTodos"
+      :clearAllCheckedTodo="clearAllCheckedTodo"
+    ></TodoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from "./components/TodoHeader";
+import TodoFooter from "./components/TodoFooter";
+import TodoList from "./components/TodoList";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoHeader,
+    TodoFooter,
+    TodoList,
+  },
+  data() {
+    return {
+      // todos: [
+      //   { id: "001", title: "学习", done: true },
+      //   { id: "002", title: "吃饭", done: false },
+      //   { id: "003", title: "睡觉", done: true },
+      // ],
+      todos: JSON.parse(localStorage.getItem("todos")) || [],
+    };
+  },
+  methods: {
+    // 增
+    addTodo(todoObject) {
+      //TODO:子-->父传值，借用方法+props实现
+      // 这里的this指向App组件实例,像es6箭头函数的this静态特性？
+      // 是的，源码这里设置了类似_that = this,固定住了
+      // 子组件TodoHeader调用时，this也指向App组件实例，
+      this.todos.unshift(todoObject);
+    },
+    // 勾选
+    checkTodo(id) {
+      this.todos.forEach((item) => {
+        if (item.id === id) {
+          item.done = !item.done;
+          return;
+        }
+      });
+    },
+    //删除
+    deleteItem(id) {
+      this.todos = this.todos.filter((item) => item.id !== id);
+    },
+    //勾选所有
+    checkAllTodos(done) {
+      this.todos.forEach((item) => (item.done = done));
+    },
+    //清除已完成的
+    clearAllCheckedTodo() {
+      this.todos = this.todos.filter((item) => !item.done);
+    },
+  },
+  watch: {
+    todos: {
+      deep: true,
+      handler(value) {
+        localStorage.setItem("todos", JSON.stringify(value));
+      },
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="todo-main">
+    <TodoItem
+      v-for="todoObj in todos"
+      :key="todoObj.id"
+      :todo="todoObj"
+      :checkTodo="checkTodo"
+      :deleteItem="deleteItem"
+    />
+  </ul>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+  name: "TodoList",
+  props: ["todos", "checkTodo","deleteItem"],
+  components: { TodoItem },
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
